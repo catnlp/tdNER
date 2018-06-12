f45c45c5a3293cefe1ba0443a1259e6faa1877f5
catnlp <wk_nlp@163.com>
2018-06-12 11:19:40 +0800

    Initialization

---
+++ main_td.py
@@ -0,0 +1,114 @@
+# encoding:utf-8
+'''
+@Author: catnlp
+@Email: wk_nlp@163.com
+@Time: 2018/6/5 12:55
+'''
+from util.config import Config
+from util.helpers import *
+
+import sys
+import argparse
+import random
+import torch
+import numpy as np
+
+seed_num = 100
+random.seed(seed_num)
+torch.manual_seed(seed_num)
+np.random.seed(seed_num)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tuning with NER')
+    parser.add_argument('--wordemb', help='Embedding for words', default='wiki')
+    # parser.add_argument('--charemb', help='Embedding for chars', default='None')
+    parser.add_argument('--status', choices=['train', 'test', 'decode'], help='update algorithm', default='decode')
+    parser.add_argument('--savemodel', default='model/intelligence/lstmcrf_bio_clean') # catnlp
+    parser.add_argument('--savedset', help='Dir of saved data setting', default='model/intelligence/lstmcrf_bio_clean.dset') # catnlp
+    parser.add_argument('--train', default='data/raw/intelligence/train.bio')
+    parser.add_argument('--test', default='data/raw/intelligence/test-clean.bio')
+    parser.add_argument('--gpu', default='True')
+    parser.add_argument('--seg', default='True')
+    parser.add_argument('--extendalphabet', default='True')
+    parser.add_argument('--raw', default='data/raw/intelligence/test/test-17.bio')
+    parser.add_argument('--loadmodel', default='/home/wk/CatNER/Pycharm/tdNER/model/intelligence/lstmcrf_bio_clean/intelligence_bio_clean')
+    parser.add_argument('--output', default='model/intelligence/decode.out')
+    args = parser.parse_args()
+
+    train_file = args.train
+    # dev_file = args.dev
+    test_file = args.test
+    raw_file = args.raw
+    model_dir = args.loadmodel
+    dset_dir = args.savedset
+    output_file = args.output
+    if args.seg.lower() == 'true':
+        seg = True
+    else:
+        seg = False
+    status = args.status.lower()
+
+    save_model_dir = args.savemodel
+    if args.gpu.lower() == 'false':
+        gpu = False
+    else:
+        gpu = torch.cuda.is_available()
+
+    print('Seed num: ', seed_num)
+    print('GPU available: ', gpu)
+    print('Status: ', status)
+
+    print('Seg: ', seg)
+    print('Train file: ', train_file)
+    # print('Dev file: ', dev_file)
+    print('Test file: ', test_file)
+    print('Raw file: ', raw_file)
+    if status == 'train':
+        print('Model saved to: ', save_model_dir)
+    sys.stdout.flush()
+
+    if status == 'train':
+        emb = args.wordemb.lower()
+        print('Word Embedding: ', emb)
+        if emb == 'wiki':
+            emb_file = 'data/embedding/wiki_100.utf8'
+        else:
+            emb_file = None
+        # char_emb_file = args.charemb.lower()
+        # print('Char Embedding: ', char_emb_file)
+
+        name = 'BaseLSTM'  # catnlp
+        config = Config()
+        config.optim = 'Adam'
+        config.lr = 0.015
+        config.hidden_dim = 200
+        config.bid_flag = True
+        config.number_normalized = False
+        data_initialization(config, train_file, test_file)
+        config.gpu = gpu
+        config.word_features = name
+        print('Word features: ', config.word_features)
+        config.generate_instance(train_file, 'train')
+        # config.generate_instance(dev_file, 'dev')
+        config.generate_instance(test_file, 'test')
+        if emb_file:
+            print('load word emb file...norm: ', config.norm_word_emb)
+            config.build_word_pretain_emb(emb_file)
+        # if char_emb_file != 'none':
+        #     print('load char emb file...norm: ', config.norm_char_emb)
+        #     config.build_char_pretrain_emb(char_emb_file)
+        name = 'intelligence_bio_clean'
+        train(config, name, dset_dir, save_model_dir, seg)
+    elif status == 'test':
+        data = load_data_setting(dset_dir)
+        # data.generate_instance(dev_file, 'dev')
+        # load_model_decode(model_dir, data, 'dev', gpu, seg)
+        data.generate_instance(test_file, 'test')
+        load_model_decode(model_dir, data, 'test', gpu, seg)
+    elif status == 'decode':
+        data = load_data_setting(dset_dir)
+        data.generate_instance(raw_file, 'raw')
+        decode_results, gold_results  = load_model_decode(model_dir, data, 'raw', gpu, seg)
+        data.write_decoded_results(output_file, gold_results, decode_results, 'raw')
+    else:
+        print('Invalid argument! Please use valid arguments! (train/test/decode)')
---
+++ module/component/MetaRNNs.py
@@ -0,0 +1,131 @@
+# encoding:utf-8
+'''
+@Author: catnlp
+@Email: wk_nlp@163.com
+@Time: 2018/4/25 21:19
+'''
+from module.component import MetaRNNCells
+
+import torch
+from torch.nn import Module
+from torch.autograd import Variable
+
+class MetaRNNBase(Module):
+    def __init__(self, mode, input_size, hidden_size, hyper_hidden_size, hyper_embedding_size, num_layers, bias=True, bias_hyper=True, gpu=False, bidirectional=False):
+        super(MetaRNNBase, self).__init__()
+        self.mode = mode
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.hyper_hidden_size = hyper_hidden_size
+        self.hyper_embedding_size = hyper_embedding_size
+        self.num_layers = num_layers
+        self.bias = bias
+        self.bias_hyper = bias_hyper
+        self.gpu = gpu
+        self.bidirectional=bidirectional
+
+        mode2cell = {'MetaRNN': MetaRNNCells.MetaRNNCell,
+                     'MetaLSTM': MetaRNNCells.MetaLSTMCell}
+
+        Cell = mode2cell[mode]
+
+        kwargs = {'input_size': input_size,
+                  'hidden_size': hidden_size,
+                  'hyper_hidden_size': hyper_hidden_size,
+                  'hyper_embedding_size': hyper_embedding_size,
+                  'bias': bias,
+                  'bias_hyper': bias_hyper}
+
+        if self.bidirectional:
+            self.cell0 = Cell(**kwargs)
+            for i in range(1, num_layers):
+                kwargs['input_size'] = hidden_size * 2
+                cell = Cell(**kwargs)
+                setattr(self, 'cell{}'.format(i), cell)
+
+            kwargs['input_size'] = input_size
+            self.cellb0 = Cell(**kwargs)
+            for i in range(1, num_layers):
+                kwargs['input_size'] = hidden_size * 2
+                cell = Cell(**kwargs)
+                setattr(self, 'cellb{}'.format(i), cell)
+        else:
+            self.cell0 = Cell(**kwargs)
+            for i in range(1, num_layers):
+                kwargs['input_size'] = hidden_size
+                cell = Cell(**kwargs)
+                setattr(self, 'cell{}'.format(i), cell)
+
+    def _initial_states(self, inputSize):
+        main_zeros = Variable(torch.zeros(inputSize, self.hidden_size))
+        meta_zeros = Variable(torch.zeros(inputSize, self.hyper_hidden_size))
+        if self.gpu:
+            main_zeros = main_zeros.cuda()
+            meta_zeros = meta_zeros.cuda()
+        zeros = (main_zeros, meta_zeros)
+        if self.mode == 'MetaLSTM':
+            states = [((main_zeros, main_zeros), (meta_zeros, meta_zeros)), ] * self.num_layers
+        else:
+            states = [zeros] * self.num_layers
+        return states
+
+    def forward(self, input):
+        states = self._initial_states(input.size(0))
+        outputs = []
+        time_steps = input.size(1)
+
+        if self.bidirectional:
+            states_b = self._initial_states(input.size(0))
+            outputs_f = []
+            outputs_b = []
+            hx = None
+
+            for num in range(self.num_layers):
+                for t in range(time_steps):
+                    x = input[:, t, :]
+                    hx = getattr(self, 'cell{}'.format(num))(x, states[num])
+                    states[num] = hx
+                    if self.mode.startswith('MetaLSTM'):
+                        outputs_f.append(hx[0][0])
+                    else:
+                        outputs_f.append(hx[0])
+                for t in range(time_steps)[::-1]:
+                    x = input[:, t, :]
+                    hx = getattr(self, 'cellb{}'.format(num))(x, states_b[num])
+                    states_b[num] = hx
+                    if self.mode.startswith('MetaLSTM'):
+                        outputs_b.append(hx[0][0])
+                    else:
+                        outputs_b.append(hx[0])
+                outputs_b.reverse()
+                input = torch.cat([torch.stack(outputs_f).transpose(0, 1), torch.stack(outputs_b).transpose(0, 1)], 2)
+                outputs_f = []
+                outputs_b = []
+            output = input, input[-1]
+        else:
+            for t in range(time_steps):
+                x = input[:, t, :]
+                for num in range(self.num_layers):
+                    hx = getattr(self, 'cell{}'.format(num))(x, states[num])
+                    states[num] = hx
+                    if self.mode.startswith('MetaLSTM'):
+                        x = hx[0][0]
+                    else:
+                        x = hx[0]
+                outputs.append(hx[0])
+
+            if self.mode.startswith('MetaLSTM'):
+                hs, cs = zip(*outputs)
+                h = torch.stack(hs).transpose(0, 1)
+                output = h, (outputs[-1][0], outputs[-1][1])
+            else:
+                output = torch.stack(outputs).transpose(0, 1), outputs[-1]
+        return output
+
+class MetaRNN(MetaRNNBase):
+    def __init__(self, *args, **kwargs):
+        super(MetaRNN, self).__init__('MetaRNN', *args, **kwargs)
+
+class MetaLSTM(MetaRNNBase):
+    def __init__(self, *args, **kwargs):
+        super(MetaLSTM, self).__init__('MetaLSTM', *args, **kwargs)
